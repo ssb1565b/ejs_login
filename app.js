8a03044c7580605ff5cf38f2d23e0cc30a41da25
dbswp <dbswp980427@gmail.com>
2023-03-13 00:49:46 +0900

    pull 받기

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = 4001
 app.use('/', (req, res) => {
   res.send('로그인페이지')
 })
-// const mainRouter = require("./routes");
+
 app.listen(PORT, () => {
   console.log(`${PORT}번에서 서버실행`)
 })
