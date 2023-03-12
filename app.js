92f5842a371fb2e453bd8378c62099b8893d1d76
ssb1565b <ssongsumovie@naver.com>
2023-03-13 00:21:12 +0900

    설정

---
+++ app.js
@@ -0,0 +1,9 @@
+// @ts-check
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const PORT = 4001;
+
+// const mainRouter = require("./routes");
